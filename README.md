5909935b5fb09a8d2878a1f5d29a146265832b55
roni69k <129597956+roni69k@users.noreply.github.com>
2024-12-04 07:14:03 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# after-login.html
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/RONI-BHANDARI/pen/OPLyQxB](https://codepen.io/RONI-BHANDARI/pen/OPLyQxB).
+
---
+++ script.js
@@ -0,0 +1,9 @@
+function contactUs() {
+  // Redirect to a contact page or display a message
+  window.location.href = 'https://telegram.me/Roni0937r'; 
+  // This will redirect to the contact page
+}
+
+function Activate() {
+  window.location.href = 'https://upilinks.in/payment-link/upi76373293'; 
+}
